3adc99233905f2ccfc05570f6a38d93bd68416f3
ericbor <iaroslav.borysov@gmail.com>
2020-11-25 20:56:10 -0500

    arrays: two numbers sum & valid subsequence

--- src/main/java/array/easy/TwoNumberSum.java
+++ src/main/java/array/easy/TwoNumberSum.java
@@ -3,14 +3,28 @@ package array.easy;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TwoNumberSum {
 
     public static int[] twoNumberSum(int[] array, int targetSum) {
+        //x+y=10 -> y=10-x
+        Set<Integer> storage = new HashSet<>();
+        for (int x : array) {
+            int y = targetSum - x;//7,5,14,2,-1,9,11
+            if (storage.contains(y)) {
+                return new int[] { x, y };
+            } else {
+                storage.add(x);//3,5,-4,8,11
+            }
+        }
 
+        return new int[2];
     }
 
     @Test
     public void verify() {
-        Assert.assertArrayEquals(new int[]{-1, 11}, twoNumberSum(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 10));
+        Assert.assertArrayEquals(new int[] { -1, 11 }, twoNumberSum(new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, 10));
     }
 }
