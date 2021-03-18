f398c2ca0291e493fcf9e82c8e007bb0747fafe7
ericbor <iaroslav.borysov@gmail.com>
2021-03-18 08:35:44 -0400

    LinkedList classic problems 2

---
+++ src/main/java/linkedlist/easy/classic/Palindrome.java
@@ -0,0 +1,82 @@
+package linkedlist.easy.classic;
+
+import linkedlist.design.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/problems/palindrome-linked-list/
+public class Palindrome {
+    //Time: O(n), Space: O(n)
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> headArray = new ArrayList<>();
+
+        while (head != null) {
+            headArray.add(head.value);
+            head = head.next;
+        }
+
+        int right = 0;
+        int left = headArray.size() - 1;
+        while (right < left) {
+            if (!headArray.get(right).equals(headArray.get(left))) {
+                return false;
+            }
+            right++;
+            left--;
+        }
+
+        return true;
+    }
+
+    public boolean isPalindrome2(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        // Find the end of first half and reverse second half
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // Check whether or not there is a palindrome.
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.value != p2.value) {
+                result = false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        // Restore the list and return the result.
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    // Taken from https://leetcode.com/problems/reverse-linked-list/solution/
+    private ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextTmp = current.next;
+            current.next = previous;
+            previous = current;
+            current = nextTmp;
+        }
+
+        return previous;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
---
+++ src/main/java/linkedlist/easy/classic/RemoveElements.java
@@ -0,0 +1,41 @@
+package linkedlist.easy.classic;
+
+import linkedlist.design.ListNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+//https://leetcode.com/problems/remove-linked-list-elements/
+public class RemoveElements {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode previous = dummy;
+        ListNode current = head;
+        while (current != null) {
+            if (current.value == val) {
+                previous.next = current.next;
+            } else {
+                previous = current;
+            }
+            current = current.next;
+        }
+
+        return dummy.next;
+    }
+
+    @Test
+    public void verify() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode result = removeElements(head, 2);
+        assertEquals(1, result.value);
+        assertEquals(3, result.next.value);
+        assertEquals(5, result.next.next.value);
+    }
+}
