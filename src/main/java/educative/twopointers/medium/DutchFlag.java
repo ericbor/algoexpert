010210aad6bc4c44fb8b780cbd841e6538085f4b
ericbor <iaroslav.borysov@gmail.com>
2021-09-22 09:16:35 -0400

    dutch flag problem

--- src/main/java/educative/twopointers/medium/DutchFlag.java
+++ src/main/java/educative/twopointers/medium/DutchFlag.java
@@ -10,21 +10,22 @@ Input: [1, 0, 2, 1, 0] ... Output: [0 0 1 1 2]
 Input: [2, 2, 0, 1, 2, 0] ... Output: [0 0 1 2 2 2 ]
  */
 public class DutchFlag {
-    public static void sort(int[] arr) {
+
+    public void sort(int[] arr) {
         int low = 0;
+        int current = 0;
         int high = arr.length - 1;
 
-        for (int i = 0; i <= high; ) {
-            if (arr[i] == 0) {
-                swap(arr, i, low);
-                i++;
+        while (current <= high) {
+            if (arr[current] == 0) {
+                swap(arr, current, low);
                 low++;
-            } else if (arr[i] == 1) {
-                i++;
-            } else { //arr[i] == 2
-                swap(arr, i, high);
-                // decrement 'high' only, after the swap the number at index 'i' could be 0, 1 or 2
+                current++;
+            } else if (arr[current] == 2) {
+                swap(arr, current, high);
                 high--;
+            } else {
+                current++;
             }
         }
     }
